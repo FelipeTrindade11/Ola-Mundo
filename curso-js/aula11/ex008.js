3a109f61fe2c0248d8f1bfee80c9e2687b1bbfff
Felipe Trindade <fe_trindade01@hotmail.com>
2021-03-09 16:55:17 -0300

    Update ex008.js

--- curso-js/aula11/ex008.js
+++ curso-js/aula11/ex008.js
@@ -1,1 +1,6 @@
-console.log('Testando o node!')
+var vel = 23.5
+console.log(`Sua velocidade é de ${vel}Km/h`)
+if (vel > 60) {
+    console.log('Você foi multado!')
+}
+console.log('Dirija sempre utilizando cinto de segurança!')
