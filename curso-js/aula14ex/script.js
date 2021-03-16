40d625d9f04c93f3f69cef8013e07b29f0fe7d65
Felipe Trindade <fe_trindade01@hotmail.com>
2021-03-15 22:05:35 -0300

    Update script.js

--- curso-js/aula14ex/script.js
+++ curso-js/aula14ex/script.js
@@ -3,30 +3,22 @@ function contar() {
     var fim = Number(window.document.getElementById('tfim').value)
     var passo = Number(window.document.getElementById('tpasso').value)
     var res = window.document.getElementById('res')
-    if (inicio >= fim || inicio <= fim) {
-        if (passo == 0 && inicio <= fim){
-            passo = 1
-            res.innerHTML = `&#128073;`
+    if (passo == 0) {
+        passo = 1
+        window.alert('Passo não pode ser 0, será considerado 1')
+    }
+    if (inicio < fim) {
+        res.innerHTML = `&#128073;`
             for (inicio; inicio <= fim; inicio += passo){
                 res.innerHTML += `${inicio} &#128073;`
             }
-            res.innerHTML += `Fim`
-        }else if (passo == 0 && inicio >= fim) {
-            passo = 1
-            res.innerHTML = `&#128073;`
-            for (inicio; inicio <= fim; inicio -= passo){
-                res.innerHTML += `${inicio} &#128073;`
-            }
-            res.innerHTML += `Fim`
-        } else {
-            res.innerHTML = `&#128073;`
-            for (inicio; inicio <= fim; inicio -= passo){
-                res.innerHTML += `${inicio} &#128073;`
-            }
-            res.innerHTML += `Fim`
-        }
+        res.innerHTML += `Fim`
     } else {
-        window.alert('[ERRO] - dados inválidos')
+        res.innerHTML = `&#128073;`
+            for (inicio; inicio >= fim; inicio -= passo){
+                res.innerHTML += `${inicio} &#128073;`
+            }
+        res.innerHTML += `Fim`
     }
 }
 
