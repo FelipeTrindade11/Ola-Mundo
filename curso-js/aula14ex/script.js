024aa1d95dc4e8735adbffe3bf7fcef362c7d444
Felipe Trindade <fe_trindade01@hotmail.com>
2021-03-15 21:50:50 -0300

    Update script.js

--- curso-js/aula14ex/script.js
+++ curso-js/aula14ex/script.js
@@ -3,11 +3,31 @@ function contar() {
     var fim = Number(window.document.getElementById('tfim').value)
     var passo = Number(window.document.getElementById('tpasso').value)
     var res = window.document.getElementById('res')
-    res.innerHTML = `&#128073;`
-    for (inicio; inicio <= fim; inicio += passo){
-        res.innerHTML += `${inicio} &#128073;`
+    if (inicio >= fim || inicio <= fim) {
+        if (passo == 0 && inicio <= fim){
+            passo = 1
+            res.innerHTML = `&#128073;`
+            for (inicio; inicio <= fim; inicio += passo){
+                res.innerHTML += `${inicio} &#128073;`
+            }
+            res.innerHTML += `Fim`
+        }else if (passo == 0 && inicio >= fim) {
+            passo = 1
+            res.innerHTML = `&#128073;`
+            for (inicio; inicio <= fim; inicio -= passo){
+                res.innerHTML += `${inicio} &#128073;`
+            }
+            res.innerHTML += `Fim`
+        } else {
+            res.innerHTML = `&#128073;`
+            for (inicio; inicio <= fim; inicio -= passo){
+                res.innerHTML += `${inicio} &#128073;`
+            }
+            res.innerHTML += `Fim`
+        }
+    } else {
+        window.alert('[ERRO] - dados inválidos')
     }
-    res.innerHTML += `Fim`
 }
 
 
